2cf54d3498182aad07dda8cad0e6038aa2f77422
edu-rojas <eduardo.rojast@sonda.com>
2019-07-25 15:07:31 -0400

    nuevo estilo baton

--- sources/Optilinker/urls.py
+++ sources/Optilinker/urls.py
@@ -13,7 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
+from baton.autodiscover import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
@@ -25,8 +26,8 @@ admin.site.index_title = 'Administración'
 admin.site.site_title = 'Linker'
 
 urlpatterns = [
-    path('grappelli/', include('grappelli.urls')),  # grappelli URLS
     path('admin/', admin.site.urls),
+    path('baton/', include('baton.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
