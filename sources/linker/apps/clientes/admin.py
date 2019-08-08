64d18330e6534726118af792993b38d86e204068
Eduardo Rojas <edurojas@MacBook-Pro-de-Eduardo.local>
2019-08-07 23:57:55 -0400

    orden

--- sources/linker/apps/clientes/admin.py
+++ sources/linker/apps/clientes/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from linker.apps.clientes.models import *
 
 # modelos de la aplicación
-admin.site.register(Clientes)
+@admin.register(Clientes)
+class ClientesAdmin(admin.ModelAdmin):
+    list_display = ('Razon_Social', 'Rut', 'Comuna', 'Direccion', 'Telefono_Primario', 'Telefono_Secundario',
+    'Encargado_Nombre', 'Encargado_Correo', 'Fecha_Creacion', 'Estado')
+    ordering = ('Fecha_Creacion',)
+    search_fields = ('Razon_Social', 'Rut', 'Comuna')
