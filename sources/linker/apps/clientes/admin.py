1a6798db2693e8b42db21bd2026031fb548f35ad
Eduardo Rojas <edurojas@MacBook-Pro-de-Eduardo.local>
2019-08-08 22:56:28 -0400

    listas

--- sources/linker/apps/clientes/admin.py
+++ sources/linker/apps/clientes/admin.py
@@ -6,6 +6,8 @@ from linker.apps.clientes.models import *
 class ClientesAdmin(admin.ModelAdmin):
     list_display = ('Razon_Social', 'Rut', 'getRegion', 'getComuna', 'Direccion', 'Telefono_Primario', 'Telefono_Secundario',
     'Encargado_Nombre', 'Encargado_Correo', 'Fecha_Creacion', 'Estado')
+    list_editable = ('Estado')
+    list_display_links = ('Razon_Social','Rut')
     ordering = ('Fecha_Creacion',)
     search_fields = ('Razon_Social', 'Rut', 'Comuna__Nombre')
 
